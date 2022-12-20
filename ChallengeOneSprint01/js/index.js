0c2ec1a97b24f83c8836d445b3908500ccc04cf2
Daniel Ramadan <44514691+DrEureka@users.noreply.github.com>
2022-12-19 23:01:31 -0300

    avances en css

---
+++ ChallengeOneSprint01/js/index.js
@@ -0,0 +1,41 @@
+import typeEffect from "./typeEffect.js"; // Importo la funcion typeEffect.
+
+const store = createStore({
+  // Creo un objeto que contiene los datos de la aplicacion.
+  input: "",
+  output: "",
+  showOutput: false,
+});
+
+/**
+ *
+ * @param {function} translateFun  // Funcion que codifica o decodifica.
+ * @returns {() => void} // Retorna una funcion.
+ */
+const encodeDecode = (translateFun) => () => {
+  // Funcion que codifica o decodifica.
+  //
+  const text = translateFun(
+    store.input
+      .get()
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "")
+  );
+
+  store.output.set(text);
+  store.showOutput.set(true);
+};
+
+document // Evento que se ejecuta cuando se escribe en el input.
+  .querySelector("#codificar")
+  .addEventListener("click", encodeDecode(ENCODER));
+
+document // Evento que se ejecuta cuando se escribe en el input.
+  .querySelector("#decodificar")
+  .addEventListener("click", encodeDecode(DECODER));
+
+document.querySelector("#copiar").addEventListener("click", () => {
+  // Evento que se ejecuta cuando se hace click en el boton copiar.
+  navigator.clipboard.writeText(store.output.get());
+  typeEffect(); // Reproduce el sonido de tipeo.
+});
